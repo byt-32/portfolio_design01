f833dab28bd234d6137e14264483b9e26b829b70
byt-32 <null@gmail.com>
2021-08-25 13:47:21 -0700

    Add transistion to toggle button on index.html script.js

--- script/script.js
+++ script/script.js
@@ -10,58 +10,31 @@
 		} else {
 			body.style.height = 'auto'
 		}
-		// if (winHeight )
+		// if (winHeight > 590) {	}
 
 	}h()
 	window.onresize = function() {
 		h()
 	};
-
-	// (function () {
-	// 	const elt = $('#mood-box')[0];
-	// 	const moods = Array.from(elt.querySelectorAll('.fa'))
-	// 	function moodSwitch() {
-	// 		const darkElts = [$('#main')]
-	// 		const lightElts = [$('#page > header'), $('#ft')]
-			
-
-	// 	}
-	// 	elt.addEventListener('click', fn)
-	// 	function fn(e) {
-	// 		function ds() {
-	// 			this.o1 = function (e, val, n) {
-	// 				setTimeout(()=>{
-	// 					e.style.display = val
-	// 				}, 300)
-	// 			}
-	// 			this.o2 = function(e, cls) {
-	// 				setTimeout(()=>{
-	// 					e.classList.remove(cls)
-	// 				}, 800)
-	// 			}
-	// 		}
-	// 		var delay = new ds()
-	// 		if (e.target == moods[0]) {
-	// 			e.target.classList.add('mvL')
-	// 			delay.o2(e.target, 'mvL') 
-	// 			moodSwitch()
-	// 		 }else{
-	// 			e.target.classList.add('mvR')
-	// 			delay.o2(e.target, 'mvR') 
-	// 		}
-			
-	// 		// var index = e.target
-	// 		if (e.target !== elt) {
-	// 			for (let i=0; i<moods.length; i+=1) {
-	// 				// ds(moods[i], 'block')()
-	// 				delay.o1(moods[i], 'block')
-	// 				if (moods[i] == e.target) {
-	// 					delay.o1(e.target, 'none')
-	// 					continue
-	// 				}
-	// 			}
-	// 		}
-			
-	// 	}
-	// })()
+	const ele = $('#mood-box'), def = $('#mood-box span');
+	// var where = 'left', fade_val=0;
+	function changeClass(initClass, newClass) {
+		setTimeout(()=>{
+			def.removeClass('fa ' + initClass)
+			def.addClass('fa ' + newClass)
+			$('#mood-box span.'+newClass).fadeIn(200)
+		}, 400)
+	}
+	ele.on('click', ()=>{
+		if (def.hasClass('fa fa-moon-o')) {
+			$('#mood-box span.fa-moon-o').fadeOut(200)
+			changeClass('fa-moon-o','fa-lightbulb-o')
+		} else {
+			$('#mood-box span.fa-lightbulb-o').fadeOut(200)
+			changeClass('fa-lightbulb-o', 'fa-moon-o')
+		}
+	})
+	function changeMood() {
+		
+	}
 })();
