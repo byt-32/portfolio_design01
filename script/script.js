10ecb3751d29e9c64477690e3d99811701f22c2b
byt-32 <null@gmail.com>
2021-08-25 14:19:54 -0700

    add mood switching

--- script/script.js
+++ script/script.js
@@ -1,14 +1,14 @@
 (function () {
+	const body = $('#main');
 	function h() {
-		const body = document.querySelector('#main');
 
 		const winHeight = window.innerHeight, 
 			winWidth = window.innerWidth;
 
 		if (winWidth > 644) {
-			body.style.height = winHeight + 'px';
+			body.css({height: winHeight})
 		} else {
-			body.style.height = 'auto'
+			body.css({height:'auto'})
 		}
 		// if (winHeight > 590) {	}
 
@@ -19,22 +19,26 @@
 	const ele = $('#mood-box'), def = $('#mood-box span');
 	// var where = 'left', fade_val=0;
 	function changeClass(initClass, newClass) {
+		$('#mood-box span.'+initClass).fadeOut(100)
 		setTimeout(()=>{
 			def.removeClass('fa ' + initClass)
 			def.addClass('fa ' + newClass)
-			$('#mood-box span.'+newClass).fadeIn(200)
+			$('#mood-box span.'+newClass).fadeIn(100)
 		}, 400)
 	}
 	ele.on('click', ()=>{
 		if (def.hasClass('fa fa-moon-o')) {
-			$('#mood-box span.fa-moon-o').fadeOut(200)
 			changeClass('fa-moon-o','fa-lightbulb-o')
+			changeMood({color: '#fff', bg: '#000'})
 		} else {
-			$('#mood-box span.fa-lightbulb-o').fadeOut(200)
 			changeClass('fa-lightbulb-o', 'fa-moon-o')
+			changeMood({color: '#000', bg: '#fff'})
 		}
 	})
-	function changeMood() {
-		
+	function changeMood(prop) {
+		body.css({background: prop.bg})
+		$('#page > header').css({color: prop.color})
+		$('#ft').css({background: prop.color})
+		$('.form-group > legend').css({color: prop.bg})
 	}
 })();
